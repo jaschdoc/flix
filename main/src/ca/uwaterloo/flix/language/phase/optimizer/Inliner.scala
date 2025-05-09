615a82edfa020d1904bc2c40ba66087bc324f878
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-05-09 13:37:14 +0200

    fix: do not handle div by zero in float

--- main/src/ca/uwaterloo/flix/language/phase/optimizer/Inliner.scala
+++ main/src/ca/uwaterloo/flix/language/phase/optimizer/Inliner.scala
@@ -521,11 +521,7 @@ object Inliner {
 
       case Float32Op.Div =>
         val List(Expr.Cst(Constant.Float32(left), tpe, _), Expr.Cst(Constant.Float32(right), _, _)) = exps
-        if (right != 0) {
           Some(Expr.Cst(Constant.Float32(left / right), tpe, loc0))
-        } else {
-          Some(Expr.Cst(Constant.Float32(0), tpe, loc0))
-        }
 
       case Float32Op.Exp =>
         val List(Expr.Cst(Constant.Float32(left), tpe, _), Expr.Cst(Constant.Float32(right), _, _)) = exps
@@ -569,11 +565,7 @@ object Inliner {
 
       case Float64Op.Div =>
         val List(Expr.Cst(Constant.Float64(left), tpe, _), Expr.Cst(Constant.Float64(right), _, _)) = exps
-        if (right != 0) {
           Some(Expr.Cst(Constant.Float64(left / right), tpe, loc0))
-        } else {
-          Some(Expr.Cst(Constant.Float64(0), tpe, loc0))
-        }
 
       case Float64Op.Exp =>
         val List(Expr.Cst(Constant.Float64(left), tpe, _), Expr.Cst(Constant.Float64(right), _, _)) = exps
