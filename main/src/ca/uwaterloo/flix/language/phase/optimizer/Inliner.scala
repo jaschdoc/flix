78dcd5d32063299c40eb83055830aea07799cdb7
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-05-09 13:10:06 +0200

    feat: inline reducible def

--- main/src/ca/uwaterloo/flix/language/phase/optimizer/Inliner.scala
+++ main/src/ca/uwaterloo/flix/language/phase/optimizer/Inliner.scala
@@ -1160,7 +1160,7 @@ object Inliner {
     */
   private def shouldInlineDef(defn: MonoAst.Def, exps: List[Expr], ctx0: LocalContext): Boolean = {
     !ctx0.currentlyInlining && !defn.spec.defContext.isSelfRef &&
-      (isSingleCall(defn.exp) || isSimple(defn.exp) || hasKnownLambda(exps))
+      (isSingleCall(defn.exp) || isSimple(defn.exp) || hasKnownLambda(exps) || hasSimpleArg(exps))
   }
 
   /**
@@ -1168,6 +1168,13 @@ object Inliner {
     */
   private def hasKnownLambda(exps: List[Expr]): Boolean = {
     exps.exists(isLambda)
+  }
+
+  /**
+    * Returns `true` if [[isSimple]] holds for at least one expression in `exps`.
+    */
+  private def hasSimpleArg(exps: List[Expr]): Boolean = {
+    exps.exists(isSimple)
   }
 
   /**
