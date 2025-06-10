4fc0ebc0d9b75d20014c7a5e24e2f7c4b00e6cfc
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-06-10 14:18:11 +0200

    fix: report correct progress info

--- main/src/ca/uwaterloo/flix/tools/BenchmarkInliner.scala
+++ main/src/ca/uwaterloo/flix/tools/BenchmarkInliner.scala
@@ -306,7 +306,6 @@ object BenchmarkInliner {
 
   private case class BenchmarkFile(private val name: String, private val opts: Options) {
     private val FileName: String = {
-      // val suffix = if (opts.xnooptimizer) "disabled" else "enabled"
       s"${name}_static_method_call"
     }
     private val JarName: String = s"$FileName.jar"
@@ -326,7 +325,7 @@ object BenchmarkInliner {
 
   private def mkScriptSnippet(file: BenchmarkFile, asprofPath: Option[String], idx: Int, programs: Map[String, (String, Int)]): String = {
     s"""rm -f ${file.OutputFile}
-       |echo "Benchmarking ${file.JarFilePath} (${idx + 1 + programs.size * 2} / ${programs.size * 4} / ${programs.size * 4})"
+       |echo "Benchmarking ${file.JarFilePath} (${idx + 1 + programs.size} / ${programs.size * 2} / ${programs.size * 2})"
        |java ${asprofPath.map(p => s"-agentpath:$p=start,fmt=collapsed,event=alloc,file=${file.ProfilingOutFile}").getOrElse("")} -jar ${file.JarFilePath} >> ${file.OutputFile}
        |""".stripMargin
   }
